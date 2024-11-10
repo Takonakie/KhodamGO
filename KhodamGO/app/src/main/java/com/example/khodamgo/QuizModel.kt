c8ebda0055852c367a393aa0394b1846d6efb58a
Takonakie <137100289+Takonakie@users.noreply.github.com>
2024-11-10 12:19:04 +0700

    Delete KhodamGO directory

--- KhodamGO/app/src/main/java/com/example/khodamgo/QuizModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.khodamgo
-
-data class QuestionModel(
-    val question : String,
-    val options : List<String>,
-    val points : List<Int>,
-){
-    constructor() : this ("", emptyList(), emptyList())
-}
