f94bac0b81f5f5a6a3a7e479646a83ccfcff2c4f
vikoadrian32 <vikosilaban1@gmail.com>
2024-12-02 23:43:32 +0700

    Update QuizModel.kt

--- KhodamGO/app/src/main/java/com/example/khodamgo/QuizModel.kt
+++ KhodamGO/app/src/main/java/com/example/khodamgo/QuizModel.kt
@@ -1,9 +1,18 @@
+
 package com.example.khodamgo
 
+data class AnswerOption(
+    val answer: String,
+    val category: String
+)
+
 data class QuestionModel(
-    val question : String,
-    val options : List<String>,
-    val points : List<Int>,
-){
-    constructor() : this ("", emptyList(), emptyList())
-}
+    val question: String,
+    val options: List<AnswerOption>,
+    val points: List<Int>
+)
+
+data class CategoryScore(
+    val category: String,
+    var points: Int = 0
+)
