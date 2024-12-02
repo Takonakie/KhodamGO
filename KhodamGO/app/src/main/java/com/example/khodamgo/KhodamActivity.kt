0e648259b6a11d6d68b32392d14f076f3c4eb8d7
vikoadrian32 <vikosilaban1@gmail.com>
2024-12-02 23:41:50 +0700

    Update KhodamActivity.kt

--- KhodamGO/app/src/main/java/com/example/khodamgo/KhodamActivity.kt
+++ KhodamGO/app/src/main/java/com/example/khodamgo/KhodamActivity.kt
@@ -4,43 +4,41 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isGone
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
 
 class KhodamActivity : AppCompatActivity() {
-
     private val CAMERA_REQUEST_CODE = 100
     lateinit var sceneView: ArSceneView
     lateinit var placeButton: ExtendedFloatingActionButton
     private lateinit var modelNode: ArModelNode
-    private var score: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_khodam)
-
-        score = intent.getIntExtra("score", 0) // Ambil skor dari Intent
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
             != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.CAMERA),
                 CAMERA_REQUEST_CODE)
         }
-
         sceneView = findViewById(R.id.sceneView)
-        placeButton = findViewById(R.id.place)
-
-        placeButton.setOnClickListener {
-            placeModel()
-        }
-
-        val modelPath = getModelPathByScore(score)
+        val khodam = intent.getStringExtra("KHODAM_RESULT")
+        val modelPath = getModelPathByScore(khodam)
         modelNode = ArModelNode().apply {
             loadModelGlbAsync(
                 glbFileLocation = modelPath
@@ -54,20 +52,21 @@ class KhodamActivity : AppCompatActivity() {
         sceneView.addChild(modelNode)
     }
 
-    private fun getModelPathByScore(score: Int): String {
-        return when {
-            score == 400 -> "models/"
-            score >= 340 -> "models/"
-            score >= 280 -> "models/"
-            score >= 220 -> "models/roger_sumatera.glb"
-            score >= 160 -> "models/sigit_rendang.glb"
-            else -> "models/skibidi_rizz.glb"
+    private fun getModelPathByScore(khodam:String?): String {
+        return when (khodam) {
+            "Khodam A"-> "models/ksatria.glb"
+            "Khodam B"-> "models/ambatron.glb"
+            "Khodam C"-> "models/mothertron.glb"
+            "Khodam D"-> "models/iblis_petarung.glb"
+            "Khodam E"-> "models/dewa_19.glb"
+            "Khodam F"-> "models/dewa_mesir.glb"
+            "Khodam G"-> "models/iblis_neraka.glb"
+            "Khodam H"-> "models/legenda_ayam.glb"
+            "Khodam I"-> "models/naga_biru.glb"
+            "Khodam J"-> "models/dewi_api.glb"
+            "Khodam K"-> "models/kepala_tiga.glb"
+            else -> "models/kepala_tiga.glb"
         }
-    }
-
-    private fun placeModel() {
-        modelNode.anchor()
-        sceneView.planeRenderer.isVisible = false
     }
 
     override fun onBackPressed() {
@@ -77,5 +76,6 @@ class KhodamActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
 }
 
